8f405db596d1298765c4c10e2ba7c307cfd50eab
RRaven <rysal.rites@legionintegrated.com>
2023-08-13 16:39:26 +0800

    Last

--- src/Pages/MainPage.jsx
+++ src/Pages/MainPage.jsx
@@ -15,7 +15,14 @@ import Ionic from "../assets/ionic.png";
 import Javascript from "../assets/js.png";
 import Mui from "../assets/mui.png";
 import Reactjs from "../assets/react.png";
+import Bootstrap from "../assets/bootstrap.png";
 
+import Linkden from "../assets/linkden.png";
+import Discord from "../assets/discord.png";
+import Facebook from "../assets/facebook.png";
+
+import Laptop from "../assets/laptop.png";
+import Mobile from "../assets/mobile.png";
 
 function MainPage() {
   const [backgroundImage, setBackgroundImage] = useState();
@@ -118,7 +125,7 @@ function MainPage() {
           backgroundPosition: "center",
           width: "100%",
           height: "100vh",
-          marginTop:"-73rem"
+          marginTop: "-141rem",
         }}
       />
       <img
@@ -132,7 +139,7 @@ function MainPage() {
           backgroundPosition: "center",
           width: "100%",
           height: "100vh",
-          marginTop:"-73rem"
+          marginTop: "-141rem",
         }}
       />
       <img
@@ -146,7 +153,7 @@ function MainPage() {
           backgroundPosition: "center",
           width: "100%",
           height: "100vh",
-          marginTop:"-73rem"
+          marginTop: "-141rem",
         }}
       />
       <img
@@ -160,7 +167,7 @@ function MainPage() {
           backgroundPosition: "center",
           width: "100%",
           height: "100vh",
-          marginTop:"-73rem"
+          marginTop: "-141rem",
         }}
       />
       <img
@@ -174,7 +181,7 @@ function MainPage() {
           backgroundPosition: "center",
           width: "100%",
           height: "100vh",
-          marginTop:"-73rem"
+          marginTop: "-141rem",
         }}
       />
       {/* developer's quote START */}
@@ -210,7 +217,7 @@ function MainPage() {
         <Col className="projects-col">
           <Row>
             <Col>
-            <p className="sampleprojecttitle">Sample Projects</p>
+              <p className="sampleprojecttitle">Sample Projects</p>
             </Col>
           </Row>
           <Row>
@@ -264,7 +271,12 @@ function MainPage() {
               >
                 <Card.Body className="project-3-cardbody">
                   <Card.Title>
-                    <p className="project-1-cardtitle" style={{ color: "white" }}>Ordering Platform</p>
+                    <p
+                      className="project-1-cardtitle"
+                      style={{ color: "white" }}
+                    >
+                      Ordering Platform
+                    </p>
                   </Card.Title>
                   {/* <Card.Text>
                     This project is a tailored fit for a company to do a
@@ -282,7 +294,7 @@ function MainPage() {
               >
                 <Card.Body className="project-4-cardbody">
                   <Card.Title>
-                    <p className="project-1-cardtitle" >Casenote-MD</p>
+                    <p className="project-1-cardtitle">Casenote-MD</p>
                   </Card.Title>
                   {/* <Card.Text>
                     A website and mobile project that offers a booking system
@@ -313,44 +325,204 @@ function MainPage() {
         </Col>
       </Row>
 
-      <Row style={{marginTop:"20rem"}}>
+      <Row style={{ marginTop: "20rem" }}>
         <Col>
-        <p className="techstacktitle">My Tech Stack</p>
-        <p className="techstacksubtitle">What Technologies and Frameworks<br/> I &apos; m Currently using and working on.</p>
+          <p className="techstacktitle">My Tech Stack</p>
+          <p className="techstacksubtitle">
+            What Technologies and Frameworks
+            <br /> I &apos; m Currently using and working on.
+          </p>
         </Col>
       </Row>
 
-      <Row style={{marginBottom:"20rem"}}>
+      <Row style={{ marginBottom: "20rem" }}>
+        <Col>
+          <Card className="techstack">
+            <Card.Body className="techstackbody">
+              <Row style={{ textAlign: "center" }}>
+                <Col>
+                  <img
+                    src={Html}
+                    style={{ width: "260px", marginTop: "1rem" }}
+                  />
+                </Col>
+                <Col>
+                  <img
+                    src={Css}
+                    style={{ width: "145px", marginTop: "1rem" }}
+                  />
+                </Col>
+                <Col>
+                  <img
+                    src={Javascript}
+                    style={{ width: "130px", marginTop: "1rem" }}
+                  />
+                </Col>
+                <Col>
+                  <img
+                    src={Bootstrap}
+                    style={{ width: "130px", marginTop: "1rem" }}
+                  />
+                </Col>
+                <Col>
+                  <img
+                    src={Reactjs}
+                    style={{ width: "250px", marginTop: "2rem" }}
+                  />
+                </Col>
+                <Col>
+                  <img
+                    src={Ionic}
+                    style={{ width: "250px", marginTop: "2rem" }}
+                  />
+                </Col>
+                <Col>
+                  <img
+                    src={Mui}
+                    style={{ width: "205px", marginTop: "1rem" }}
+                  />
+                </Col>
+              </Row>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+
+      <Row className="rowinformation">
+        <Col>
+          <Row>
             <Col>
-            <Card
-                className="techstack"
-              >
-                <Card.Body className="techstackbody">
-                  <Row style={{textAlign:"center"}}>
-                    <Col>
-                    <img src={Html} style={{width:"260px",marginTop:"1rem"}}/>
-                    </Col>
-                    <Col>
-                    <img src={Css} style={{width:"145px",marginTop:"1rem"}}/>
-                    </Col>
-                    <Col>
-                    <img src={Javascript} style={{width:"130px",marginTop:"1rem"}}/>
-                    </Col>
-                    <Col>
-                    <img src={Reactjs} style={{width:"250px",marginTop:"2rem"}}/>
-                    </Col>
-                    <Col>
-                    <img src={Ionic} style={{width:"250px",marginTop:"2rem"}}/>
-                    </Col>
-                    <Col>
-                    <img src={Mui} style={{width:"205px",marginTop:"1rem"}}/>
-                    </Col>
-                  </Row>
-                 
-                </Card.Body>
-              </Card>
+              <p className="infotitle"> Contact Informations</p>
+              <div className="divideline"></div>
+              <Row>
+                <Col>
+                  <img
+                    src={Linkden}
+                    style={{ width: "150px", marginTop: "1rem", cursor:"pointer" }}
+                    onClick={() => window.location.assign("https://www.linkedin.com/in/rysal-raven-rites-b342a6250/")}
+                  />
+                </Col>
+                <Col>
+                  <img
+                    src={Discord}
+                    style={{ width: "200px", marginTop: "0rem", cursor:"pointer" }}
+                    onClick={() => window.location.assign("https://discordapp.com/users/blacklotus3022")}
+                  />
+                </Col>
+                <Col>
+                  <img
+                    src={Facebook}
+                    style={{ width: "150px", marginTop: "1rem", cursor:"pointer" }}
+                    onClick={() => window.location.assign("https://www.facebook.com/profile.php?id=100001236377376")}
+                  />
+                </Col>
+              </Row>
+            </Col>
+
+            <Col>
+              <p className="infotitle">Experiences</p>
+              <div className="divideline"></div>
+              <p className="infosubtitle">Software Developer</p>
+              <Row>
+                <Col>
+                  <p className="infotext">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="30"
+                      height="30"
+                      fill="currentColor"
+                      className="bi bi-building"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M4 2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1ZM4 5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1ZM7.5 5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1Zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1ZM4.5 8a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1Zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1Z" />
+                      <path d="M2 1a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V1Zm11 0H3v14h3v-2.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5V15h3V1Z" />
+                    </svg>
+                    &nbsp;&nbsp;LegionTech INC.
+                  </p>
+                </Col>
+                <Col>
+                  <p className="infotext">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="30"
+                      height="30"
+                      fill="currentColor"
+                      className="bi bi-clipboard-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10 1.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-1Zm-5 0A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5v1A1.5 1.5 0 0 1 9.5 4h-3A1.5 1.5 0 0 1 5 2.5v-1Zm-2 0h1v1A2.5 2.5 0 0 0 6.5 5h3A2.5 2.5 0 0 0 12 2.5v-1h1a2 2 0 0 1 2 2V14a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V3.5a2 2 0 0 1 2-2Z"
+                      />
+                    </svg>
+                    &nbsp;&nbsp;Internship
+                  </p>
+                </Col>
+                <Col>
+                  <p className="infotext">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="30"
+                      height="30"
+                      fill="currentColor"
+                      className="bi bi-calendar-event-fill"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M4 .5a.5.5 0 0 0-1 0V1H2a2 2 0 0 0-2 2v1h16V3a2 2 0 0 0-2-2h-1V.5a.5.5 0 0 0-1 0V1H4V.5zM16 14V5H0v9a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2zm-3.5-7h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5z" />
+                    </svg>
+                    &nbsp;&nbsp;September 2022
+                  </p>
+                </Col>
+              </Row>
+              <div className="divideline"></div>
+              <p className="infosubtitle">Education Background</p>
+              <p className="infotext">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="currentColor"
+                  className="bi bi-buildings"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022ZM6 8.694 1 10.36V15h5V8.694ZM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5V15Z" />
+                  <path d="M2 11h1v1H2v-1Zm2 0h1v1H4v-1Zm-2 2h1v1H2v-1Zm2 0h1v1H4v-1Zm4-4h1v1H8V9Zm2 0h1v1h-1V9Zm-2 2h1v1H8v-1Zm2 0h1v1h-1v-1Zm2-2h1v1h-1V9Zm0 2h1v1h-1v-1ZM8 7h1v1H8V7Zm2 0h1v1h-1V7Zm2 0h1v1h-1V7ZM8 5h1v1H8V5Zm2 0h1v1h-1V5Zm2 0h1v1h-1V5Zm0-2h1v1h-1V3Z" />
+                </svg>
+                &nbsp;&nbsp;Bachelor of Science in Computer Science
+              </p>
             </Col>
           </Row>
+        </Col>
+      </Row>
+
+      <Row style={{ marginTop: "10rem" }} className="rowlast">
+        <Col style={{ marginRight: "25rem" }}>
+          <img src={Laptop} style={{ width: "150px", marginTop: "1rem" }} />
+          <div className="dividelast"></div>
+          <p className="infotextlast">
+            Creating Responsive Website that can be use wheter it is in Computer
+            <br />
+            or Mobile Devices. Building Web Application using Html, Css,
+            <br />
+            JavaScript, BootStrap and React JS.
+          </p>
+        </Col>
+        <Col>
+          <img src={Mobile} style={{ width: "150px", marginTop: "1rem" }} />
+          <div className="dividelast"></div>
+          <p className="infotextlast">
+            Creating a responsive coding application that can be utilized across
+            various <br />
+            mobile devices in the market involves developing such applications
+            using a <br />
+            multi-platform framework, such as the Ionic Framework. This approach
+            allows <br />
+            for deploying the application on both Android and iOS platforms
+            effectively <br />
+            utilizing the capabilities of the Ionic Framework.
+          </p>
+        </Col>
+      </Row>
 
       {/* developer's Projects END */}
     </Container>
